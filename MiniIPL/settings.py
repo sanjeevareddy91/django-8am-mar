a74513b22098b9202f3c3458ece75bab97a11dbe
sanjeevareddy91 <gsanjeevreddy91@gmail.com>
2023-03-29 09:37:24 +0530

    29-03-2023 class files pushed

--- MiniIPL/settings.py
+++ MiniIPL/settings.py
@@ -150,6 +150,6 @@ MESSAGE_TAGS = {
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'gsanjeevreddy91@gmail.com'
-EMAIL_HOST_PASSWORD = 'zskkmykfmcceqgax'
+EMAIL_HOST_USER = 'your email'
+EMAIL_HOST_PASSWORD = 'your app password'
 EMAIL_USE_TLS = True
