bab197e381739abc69face2a7d4321f3ddb2d9d3
Sanjeeva Reddy <31734152+sanjeevareddy91@users.noreply.github.com>
2023-04-11 21:55:39 +0530

    Updated serializer

--- iplapp/serializers.py
+++ iplapp/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
+
 from .models import Team_Name
 
-class TeamNameSerializer(serializers.ModelSerializer):
+class TeamNameModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Team_Name
         fields = "__all__"
